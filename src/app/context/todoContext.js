083f93aa7f6be476f544585fa4cd224daa9b7bc4
redarashid <redarashid824@gmail.com>
2025-07-25 18:31:20 +0300

    first commit

---
+++ src/app/context/todoContext.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const todoContext = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default todoContext
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,21 @@
+import Button from "./components/button";
+import { LuPlus } from "react-icons/lu";
+import { useTodos } from "../app/context/todoContext";
 
 export default function Home() {
+  const { openTodoModal, setOpenTodoModal } = useTodos();
+  const closeTodoModal = () => {
+    setOpenTodoModal(false);
+  };
   return (
-    <div className=" flex items-center justify-center h-screen font-extrabold text-4xl">
-      Wlcom to page
+    <div className=" p-4">
+      <div className=" flex flex-wrap gap-4 md:gap-3 justify-between items-center mb-3 md:mb-4">
+        <h1 className=" text-lg md:text-2xl font-semibold">My Tasks</h1>
+        <Button onClick={() => setOpenTodoModal(true)}>
+          <LuPlus className=" mr-2 text-xl" />
+          Add Task
+        </Button>
+      </div>
     </div>
   );
 }
