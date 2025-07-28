5f7b8f2da1d11924b2507b507ecfada94738c2f4
redarashid <redarashid824@gmail.com>
2025-07-28 12:19:39 +0300

    first commit

--- src/app/components/themeToggle.js
+++ src/app/components/themeToggle.js
@@ -1,9 +1,36 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useState } from "react";
 
-const themeToggle = () => {
+const ThemeToggle = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  // 
+  useEffect(() => {
+    const saveTheme = localStorage.getItem("theme");
+    if (
+      saveTheme === "dark" ||
+      (!saveTheme && window.matchMedia(`(prefers-color-scheme: dark)`).matches)
+    ) {
+      document.documentElement.classList.add("dark");
+      setIsDarkMode(true);
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, []);
+
+  const themeToggle = () => {
+    const root = document.documentElement;
+    const newTheme = isDarkMode ? "light" : "dark";
+    setIsDarkMode(!isDarkMode);
+    root.classList.toggle("dark", !isDarkMode);
+    localStorage.setItem("theme", newTheme);
+  };
+
   return (
-    <div>themeToggle</div>
-  )
-}
+    <div className=" flex items-center gap-2">
+      <div></div>
+    </div>
+  );
+};
 
-export default themeToggle
+export default ThemeToggle;
