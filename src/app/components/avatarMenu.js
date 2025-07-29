41614fdfa77a9abdbadc40a3a86536d9522bdd72
redarashid <redarashid824@gmail.com>
2025-07-29 17:35:41 +0300

    first commit

--- src/app/components/avatarMenu.js
+++ src/app/components/avatarMenu.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
-const avatarMenu = () => {
+const AvatarMenu = () => {
   return (
-    <div>avatarMenu</div>
-  )
-}
+    <div className=" relative z-20">
+      <Image
+        src={`${process.env.NEXT_PUBLIC_BASE_PATH || ""}/images/profile.jpg`}
+        alt="profile"
+        width={40}
+        height={40}
+        className=" w-10 h-10 rounded-full bg-gray-500 cursor-pointer"
+        unoptimized
+      />
+    </div>
+  );
+};
 
-export default avatarMenu
+export default AvatarMenu;
