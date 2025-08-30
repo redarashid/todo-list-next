faeb1ee8ebd22d92a575cd06330597a5e1ac4d04
redarashid <redarashid824@gmail.com>
2025-08-30 16:41:51 +0300

    first commit

--- components/deleteConfirmationModal.js
+++ components/deleteConfirmationModal.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { LuAlertCircle } from "react-icons/lu";
+import { IoAlertCircleOutline } from "react-icons/io5";
 import Modal from "./modal";
 import Button from "./button";
 
@@ -10,7 +10,7 @@ const DeleteConfirmationModal = ({ isOpen, onClose, onDelete, itemName }) => {
     <Modal isOpen={true} onClose={onClose}>
       <div className="p-4">
         <h2 className="text-xl text-orange-500 font-bold inline-flex gap-3 mb-3 items-center">
-          <LuAlertCircle /> Delete Confirmation
+          <IoAlertCircleOutline /> Delete Confirmation
         </h2>
         <p className="mb-4">Are you sure you want to delete "{itemName}"?</p>
         <div className="flex justify-end gap-4">
